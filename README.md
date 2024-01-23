b3500d986dc9cb87f223592f3d65cbd8e00f335c
Your Name <you@example.com>
2024-01-23 07:07:41 +0000

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Setup script for CodeCatalyst Dev Environment 
+
+This repository includes a simple setup script for configuring the codecatalyst development environment to make it convenient. The setup script automates the installation of commonly used tools and configurations.
+
+
+## Usage
+
+To set up the environment, run the following commands:
+
+```sh
+git clone https://github.com/darma29/codecatalyst-devenv-setup.git && cd codecatalyst-devenv-setup && chmod +x setup.sh && ./setup.bash
+
+```
+
+## Tools
+
+The setup script installs the following tools:
+
+- **[Starship](https://starship.rs/):** A minimal, blazing-fast, and infinitely customizable prompt for any shell.
+- **[The Silver Searcher (ag)](https://github.com/ggreer/the_silver_searcher):** A fast code searching tool.
+- **[Lazygit](https://github.com/jesseduffield/lazygit):** A simple terminal UI for Git commands.
---
+++ setup.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Enable debug mode
+set -x
+
+# Install starship
+echo -e "\n" | curl -sS https://starship.rs/install.sh | sh -s -- -y
+
+# Install the_silver_searcher
+sudo dnf -y groupinstall "Development Tools" && \
+sudo dnf -y install pcre-devel xz-devel zlib-devel && \
+cd /usr/local/src && \
+[ -d the_silver_searcher ] || sudo git clone https://github.com/ggreer/the_silver_searcher.git && \
+cd the_silver_searcher/ && \
+sudo ./build.sh && \
+sudo make install
+
+# Install lazygit
+cd /tmp && \
+[ -d lazygit ] || git clone https://github.com/jesseduffield/lazygit.git && \
+cd lazygit && \
+go install
+
+# Update .bashrc
+echo '
+### Custom script for codecatalyst ###
+# git completion
+source /usr/share/doc/git/contrib/completion/git-completion.bash
+# Install starship: curl -sS https://starship.rs/install.sh | sh
+eval "$(starship init bash)"
+# Install lazygit : go install github.com/jesseduffield/lazygit@latest
+go_path="~/go/bin"
+if [[ ":$PATH:" != *":$go_path:"* ]]; then
+    export PATH=$PATH:$go_path # Check if the path is not already in the PATH
+fi
+' >> ~/.bashrc
+
+# Display completion message
+echo "Environment setup completed!"
+
+# Disable debug mode
+set +x
